05829646a2d9ccbeb10f849a733c47f65fa76190
Aluno Invest <aluno.invest@invest.local>
2022-09-21 22:09:53 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+
+import './App.css'
+
+import React, {
+//  useState,
+} from 'react'
+
+import Home from './section/home'
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Home />
+      
+      
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/section/home/index.jsx
@@ -0,0 +1,42 @@
+
+import fundo from '../../assets/fundo2.jpg';
+import foto from '../../assets/fotominha.jpg';
+
+import './home.css'
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+export default function Home() {
+
+  return (
+    <div id='home' className='home'>
+      <div className="imgHome">
+        <img src={fundo} alt='ImagemFundo' className='ImagenFundo' />
+        <div className='todo'>
+          <div className='title'>
+            <h1>Bem Vindo</h1>
+          </div>
+          <div className='title2'>
+            <h1>Portifolio Jardel Silva</h1>
+          </div>
+          <div className='iconsTodo' >
+            <div className='linkedin'>
+              <FaLinkedin size={80} />
+            </div>
+            <div className='instagram'>
+              <FaInstagram size={80} />
+            </div>
+            <div className='github'>
+              <FaGithub size={80} />
+            </div>
+          </div>
+          <img  src={foto} className='fotoperfil'/>
+        </div>
+
+
+
+      </div>
+    </div>
+  )
+}
